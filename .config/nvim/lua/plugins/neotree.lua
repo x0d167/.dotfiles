21af5a480f37e94d4392c7f0cfcdceb191133553
x0d <steko415@proton.me>
2024-09-18 17:43:15 -0500

    still fixing neotree

--- .config/nvim/lua/plugins/neotree.lua
+++ .config/nvim/lua/plugins/neotree.lua
@@ -316,19 +316,6 @@ return {
             ['ot'] = { 'order_by_type', nowait = false },
           },
         },
-        symbols = {
-          -- Change type
-          added = '', -- or "✚", but this is redundant info if you use git_status_colors on the name
-          modified = '', -- or "", but this is redundant info if you use git_status_colors on the name
-          deleted = '✖', -- this can only be used in the git_status source
-          renamed = '󰁕', -- this can only be used in the git_status source
-          -- Status type
-          untracked = '',
-          ignored = '',
-          unstaged = '󰄱',
-          staged = '',
-          conflict = '',
-        },
       },
     }
 
