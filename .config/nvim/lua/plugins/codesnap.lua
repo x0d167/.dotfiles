6e56c9c02e8cefdebfe96553ff77ce171c025956
x0d <steko415@proton.me>
2025-04-09 22:42:15 -0500

    added kitty config and more nvim and alacritty tweaks.

--- .config/nvim/lua/plugins/codesnap.lua
+++ .config/nvim/lua/plugins/codesnap.lua
@@ -1,21 +1,21 @@
 return {
   "mistricky/codesnap.nvim",
   event = "VeryLazy",
-  build = "make build_generator",
+  build = "make",
   keys = {
-    { "<leader>cc", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
+    { "<leader>cy", "<cmd>CodeSnap<cr>", mode = "x", desc = "Save selected code snapshot into clipboard" },
     { "<leader>cs", "<cmd>CodeSnapSave<cr>", mode = "x", desc = "Save selected code snapshot in ~/Pictures" },
   },
   opts = {
     save_path = "~/Pictures/CodeSnap",
-    has_breadcrumbs = true,
-    has_line_number = true,
-    bg_theme = "default",
+    has_breadcrumbs = false,
+    has_line_number = false,
+    bg_theme = "summer",
     bg_padding = 0,
     border = "rounded",
-    watermark = "x0d",
+    watermark = "x0d1",
     watermark_font_family = "MesloLG",
     mac_window_bar = false,
-    show_workspace = true,
+    show_workspace = false,
   },
 }
