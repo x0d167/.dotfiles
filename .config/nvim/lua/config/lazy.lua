88d35071479a1322e286ac4a6a887dcfe021802f
x0d <steko415@proton.me>
2025-04-16 20:17:24 -0500

    transparent nvim, more opacity kitty, new aliases .bashrc

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -51,3 +51,15 @@ require("lazy").setup({
     },
   },
 })
+
+-- Transparency setup (place this towards the end)
+vim.cmd([[
+  hi NonText ctermbg=none guibg=NONE
+  hi Normal guibg=NONE ctermbg=NONE
+  hi NormalNC guibg=NONE ctermbg=NONE
+  hi SignColumn ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi Pmenu ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi FloatBorder ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi NormalFloat ctermbg=NONE ctermfg=NONE guibg=NONE
+  hi TabLine ctermbg=None ctermfg=None guibg=None
+]])
