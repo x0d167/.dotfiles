0e2d21c3c500938fd706bf9783a8ea51cf64000a
x0d <steko415@proton.me>
2025-04-22 22:50:01 -0500

    added some major changes to bashrc config, to make it more modular

---
+++ config/.config/bashrc/init.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+
+# init.sh
+
+# Exit early if not interactive
+if [[ $- == *i* ]]; then
+  export iatest=1
+else
+  export iatest=0
+fi
+
+# Run fastfetch if available
+command -v fastfetch &>/dev/null && fastfetch
+
+# Source system bash configs (optional)
+[ -f /etc/bashrc ] && source /etc/bashrc
+
+# Enable programmable completion (Bash only)
+if [ -f /usr/share/bash-completion/bash_completion ]; then
+  source /usr/share/bash-completion/bash_completion
+elif [ -f /etc/bash_completion ]; then
+  source /etc/bash_completion
+fi
---
+++ config/.config/bashrc/prompt.sh
@@ -0,0 +1,14 @@
+#!/usr/bin/env bash
+
+# Ctrl+F inserts 'zi' in interactive shells
+if [[ $- == *i* ]]; then
+  bind '"\C-f":"zi\n"'
+fi
+
+# Prompt tool setup (Starship or Oh My Posh)
+# eval "$(starship init bash)"
+eval "$(oh-my-posh init bash --config ~/.config/ohmyposh/EDM115-newline.omp.json)"
+
+eval "$(uv generate-shell-completion bash)"
+eval "$(uvx --generate-shell-completion bash)"
+eval "$(zoxide init --cmd cd bash)"
