c4ecfc8adac3050928ea47838a0a1f6d9fe3462e
x0d <steko415@proton.me>
2025-05-01 18:19:41 -0500

    mild nvim, rio, and kitty tweaks.

--- config/.config/nvim/lua/plugins/fzf-lua.lua
+++ config/.config/nvim/lua/plugins/fzf-lua.lua
@@ -186,7 +186,7 @@ return {
     { "<leader>gc", "<cmd>FzfLua git_commits<CR>", desc = "Commits" },
     { "<leader>gs", "<cmd>FzfLua git_status<CR>", desc = "Status" },
     -- search
-    { '<leader>s"', "<cmd>FzfLua registers<cr>", desc = "Registers" },
+    { "<leader>s", "<cmd>FzfLua registers<cr>", desc = "Registers" },
     { "<leader>sa", "<cmd>FzfLua autocmds<cr>", desc = "Auto Commands" },
     { "<leader>sb", "<cmd>FzfLua grep_curbuf<cr>", desc = "Buffer" },
     { "<leader>sc", "<cmd>FzfLua command_history<cr>", desc = "Command History" },
